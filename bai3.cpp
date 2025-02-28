5457fb24900e3665cc02d4801cf394fbf4b11ef8
Hung-tth <22029095@vnu.edu.vn>
2025-02-28 22:24:52 +0700

    Add files via upload

---
+++ bai3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int count_triplets(int arr[], int n) {
+    if (n < 3) return 0; 
+
+    int count = 0;
+    for (int i = 1; i < n - 1; i++) {
+        if (arr[i - 1] + arr[i] + arr[i + 1] == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int arr[n]; 
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i]; 
+    }
+
+   cout << count_triplets(arr, n) << endl;
+
+    return 0;
+}
+
